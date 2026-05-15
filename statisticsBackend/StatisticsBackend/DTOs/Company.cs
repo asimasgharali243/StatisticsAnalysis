d859ade5179a427dc5780d05c8870a44461320c7
asimasgharali243 <asimasgharali243@gmail.com>
2026-05-15 18:18:20 +0500

    final clean structure

---
+++ statisticsBackend/StatisticsBackend/DTOs/Company.cs
@@ -0,0 +1,8 @@
+// Models/Company.cs
+namespace StatisticsBackend.DTOs;
+
+public class Company
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
