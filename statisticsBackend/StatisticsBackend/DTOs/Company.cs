70a44366172c89bca9167f67f42b13f19de7f996
asimasgharali243 <asimasgharali243@gmail.com>
2026-05-15 18:21:45 +0500

    remove all files from remote

--- statisticsBackend/StatisticsBackend/DTOs/Company.cs
+++
@@ -1,8 +0,0 @@
-// Models/Company.cs
-namespace StatisticsBackend.DTOs;
-
-public class Company
-{
-    public string Id { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-}
